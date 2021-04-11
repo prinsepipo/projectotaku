df552fe0ae6b9ae0fb0d69c51ed01466d967445e
Pipo <prinsepipo.sanchez@gmail.com>
2021-04-11 10:56:12 +0800

    Implement delete item in watchlist

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas, faSearch } from '@fortawesome/free-solid-svg-icons'
+import { fas, faSearch, faTrash } from '@fortawesome/free-solid-svg-icons'
 
 import './index.css';
 import App from './App';
 
 
-library.add(fas, faSearch);
+library.add(fas, faSearch, faTrash);
 
 ReactDOM.render(
   <React.StrictMode>
--- webapp/src/pages/watchlist/kanban/KanbanListItem.js
+++ webapp/src/pages/watchlist/kanban/KanbanListItem.js
@@ -1,12 +1,69 @@
+import { useContext, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import { Draggable } from 'react-beautiful-dnd';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import WatchlistContext from '../../../context/WatchlistContext';
+
+import { backendAPI } from '../../../utils/api';
 
 
 function KanbanListItem(props) {
+    const [watchlist, setWatchlist] = useContext(WatchlistContext);
+    const [modifier, setModifier] = useState('');
+    const history = useHistory();
+
+    const deleteItem = (e) => {
+        const list = {...watchlist};
+        const itemIndex = list[props.item.status].findIndex((element) => element.id === props.item.id);
+        const prevItem = list[props.item.status][itemIndex - 1];
+        const nextItem = list[props.item.status][itemIndex + 1];
+
+        if (prevItem) {
+            prevItem.next_item_id = nextItem ? nextItem.id : null;
+
+            const headers = { Authorization: `Token ${localStorage.getItem('TOKEN')}` };
+            backendAPI.put(`watchlist/${prevItem.id}/`, prevItem, {headers})
+                .catch((error) => {
+                    if (error.response) {
+                        if (error.response.status === 401) {
+                            history.push('/logout');
+                        } else {
+                            console.log(error.response.data);
+                        }
+                    } else {
+                        history.push('/server-error');
+                    }
+                });
+        }
+
+        setModifier('shrink')
+
+        list[props.item.status].splice(itemIndex, 1);
+
+        setTimeout(() => setWatchlist(list), 500);
+
+        const headers = { Authorization: `Token ${localStorage.getItem('TOKEN')}` };
+        backendAPI.delete(`watchlist/${props.item.id}/`, {headers})
+            .catch((error) => {
+                if (error.response) {
+                    if (error.response.status === 401) {
+                        history.push('/logout');
+                    } else {
+                        console.log(error.response.data);
+                    }
+                } else {
+                    history.push('/server-error');
+                }
+            });
+    }
+
     return (
         <Draggable draggableId={props.item.id.toString()} index={props.index}>
             {(provided) => (
                 <div
-                    className='KanbanListItem'
+                    className={'KanbanListItem KanbanListItem--' + modifier}
                     ref={provided.innerRef}
                     {...provided.draggableProps}
                     {...provided.dragHandleProps}
@@ -17,6 +74,9 @@ function KanbanListItem(props) {
                         alt={props.item.title}
                     />
                     <p className='KanbanListItem-title'>{props.item.title}</p>
+                    <button className='KanbanListItem-delete' onClick={deleteItem}>
+                        <FontAwesomeIcon icon='trash' />
+                    </button>
                 </div>
             )}
         </Draggable>
