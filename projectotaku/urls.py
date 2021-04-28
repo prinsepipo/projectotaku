00fcf2eaad3d14e96caa37da0aa0ef83bd2c6a39
Pipo <prinsepipo.sanchez@gmail.com>
2021-04-28 22:07:51 +0800

    Fix react routing not working in django urls
    By using regex, react routing will now work with django urls without
    overwriting other django urls like api or admin.

--- projectotaku/urls.py
+++ projectotaku/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html')),
+    # path('', TemplateView.as_view(template_name='index.html')),
+    re_path('^(?!(api|admin)).*$', TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
     path('api/auth/', include('account.urls')),
     path('api/watchlist/', include('watchlist.urls')),
