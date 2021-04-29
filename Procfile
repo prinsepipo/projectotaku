c62d37d4138c98c7c7f36327585c77d9b37c754d
Pipo <prinsepipo.sanchez@gmail.com>
2021-04-29 10:01:02 +0800

    Deploy to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn projectotaku.wsgi
--- projectotaku/settings/prod.py
+++ projectotaku/settings/prod.py
@@ -1,5 +1,7 @@
 from .base import *
 
+import django_heroku
+
 
 SECRET_KEY = config('SECRET_KEY')
 
@@ -7,4 +9,34 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
+INSTALLED_APPS += [
+    'storages',
+]
+
 DATABASES = {}
+
+
+# AWS S3
+
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+
+AWS_LOCATION = 'static'
+
+
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+
+# Activate Django-Heroku
+django_heroku.settings(locals(), staticfiles=False)
